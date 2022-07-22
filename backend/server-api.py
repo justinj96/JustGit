4c10763c9e5ed3b186579f04c737118567998cb0
Justin Jacobson <justinkjacobson@yahoo.com>
2022-07-22 16:20:36 -0700

    retrieving files

--- backend/server-api.py
+++ backend/server-api.py
@@ -8,16 +8,44 @@ con.connect(host, username='ubuntu')
 
 stdin, stdout, stderr = con.exec_command('cd /srv/git/justgit.git && git ls-tree HEAD')   
    
-files = []
+files = {}
 # process the output   
 if stderr.read() == b'':   
-    for line in stdout.readlines():   
-        files.append(line)
-         # strip the trailing line breaks   
+    for line in stdout.readlines(): 
+        lst = line[7:].split('\t')
+        file_type, commit = lst[0].split(' ')
+        name = lst[1].strip()
+        files[commit] = {
+            'file_type': file_type,
+            'name': name
+        }
 else:   
     print(stderr.read())  
-stdin.close()
 
-for fle in files:
-    item = fle.strip().split('\t')[1]
-    print(item)
+sub_files = {}
+
+for parent_commit, dct in files.items():
+    if dct['file_type'].lower().strip() == 'tree':
+        stdin, stdout, stderr = con.exec_command(f'cd /srv/git/justgit.git && git ls-tree {parent_commit}')   
+        if stderr.read() == b'':
+            for line in stdout.readlines(): 
+                lst = line[7:].split('\t')
+                file_type, commit = lst[0].split(' ')
+                name = lst[1].strip()
+                if 'children' in files[parent_commit]:
+                    files[parent_commit]['children'].update({
+                        commit: {
+                        'file_type': file_type,
+                        'name': name
+                    }})
+                else:
+                    files[parent_commit].update({'children': {
+                        commit: {
+                        'file_type': file_type,
+                        'name': name
+                    }}})
+print(files)
+stdin.close()
+stdout.close()
+stderr.close()
+con.close()
